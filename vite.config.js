598991473d89f898ce2aca12e46c15af63d497b1
Nicolas97431 <nickogsm@gmail.com>
2024-07-15 22:39:24 +0400

    modif deploy

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Nicolas-BIENES-Portfolio/", // Remplace 'portfolio' par le nom de ton dépôt GitHub
+  base: "/Nicolas-BIENES-Portfolio/",
   plugins: [react()],
 });
