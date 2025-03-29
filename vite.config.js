be65e3025dc8012a45c8b293d60c08b65e1c3ab1
Nicolas97431 <nickogsm@gmail.com>
2025-03-29 19:50:53 +0400

    Deploy sur vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Nicolas-BIENES-Portfolio/",
   plugins: [react()],
+  base: "/", //"/Nicolas-BIENES-Portfolio/",
+  assetsInclude: [
+    "**/*.jpg",
+    "**/*.jpeg",
+    "**/*.png",
+    "**/*.gif",
+    "**/*.svg",
+    "**/*.JPG",
+  ],
 });
